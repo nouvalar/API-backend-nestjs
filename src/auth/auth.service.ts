622170abe7325ba1c9ee4a28204b4677d3bd73c4
zulfikar4568 <isnaen70@gmail.com>
2024-07-21 15:24:36 +0700

    init

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  signup() {
+    return 'Saya sedang signup';
+  }
+  signin() {
+    return 'Saya sedang signin';
+  }
+}
